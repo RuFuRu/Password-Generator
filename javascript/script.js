d33cb30d3068add6aa9c8a660b681a0cc75afa12
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-04-21 21:42:01 +0200

    connected rangeInput value with numInput value

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,16 @@
 const rangeInput = document.querySelector('.range-div input[type = "range"]');
 const numInput = document.querySelector('.range-div input[type = "number"]');
+const radioAscii = document.querySelector('#ascii');
+const utf16 = document.querySelector('#utf16');
+const display = document.querySelector('.display');
+const button = document.querySelector('button');
+
+numInput.value = rangeInput.value
+
+rangeInput.addEventListener('input', () => {
+    numInput.value = rangeInput.value;
+})
+
+numInput.addEventListener('input', () => {
+    rangeInput.value = numInput.value;
+})
